5d7b8047386258c6a49f40e7cac1b9611560e174
crackdba <139887833+crackdba@users.noreply.github.com>
2023-07-18 15:52:39 +0100

    Update streamlit_app.py
    change title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
