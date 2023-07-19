34cb63e88de9c85faddf593bf464e4401e7e0fa8
crackdba <139887833+crackdba@users.noreply.github.com>
2023-07-19 15:26:10 +0100

    Update streamlit_app.py
    fc2

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,8 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header ("The fruit load list contains:")
 streamlit.dataframe (my_data_rows)
+
+
+#New Section to add fruit
+fruit_choice2 = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+streamlit.write('Thanks for adding ', fruit_choice2)
